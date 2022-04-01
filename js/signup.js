05c987417170b77af38b6c719caa37c80ba394d7
ITPS <ITPS@gmail.com>
2022-04-01 15:31:09 +0900

    2022년 4월 1일 수업시간에 인스타 회원가입 페이지 만들고 자바 스크립트까지 먹여보기

---
+++ js/signup.js
@@ -0,0 +1,42 @@
+const input_datas = document.querySelectorAll('.input-data');
+const password_visibel = document.querySelector('.password-visible');
+const input_type = document.querySelector('#password');
+
+password_visibel.onclick = () => {
+    if (input_type.type == 'password') {
+        input_type.type = 'text';
+        password_visibel.innerHTML = '숨기기';
+    } else {
+        input_type.setAttribute('type', 'password');
+        password_visibel.innerHTML = '비밀번호 표시';
+    }
+}
+
+for (let i = 0; i < input_datas.length; i++) {
+    const label = input_datas[i].querySelector('label')
+    const input = input_datas[i].querySelector('input')
+
+    input.onkeydown = () => {
+        label.style.top = '2px';
+        label.style.fontSize = '11px';
+        input.style.padding = '16px 0px 2px 8px';
+
+    }
+
+    input.onkeyup = () => {
+        if (input.value.length == 0) {
+            label.style.top = '9px';
+            label.style.fontSize = '13px';
+            input.style.padding = '9px 0px 7px 8px';
+        }
+    }
+
+    input.onblur = () => {
+        const inputMsg = document.querySelectorAll('.input-msg');
+        if (input.value.length == 0) {
+            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+        } else {
+            inputMsg[i].innerHTML = '<i class="fa-solid fa-circle-check" style="color: #8e8e8e;"></i>';
+        }
+    }
+}
